4a87bf9c75c67892aab82fd57116e35fe0d30c83
CaSilicate <casilicate@outlook.com>
2025-09-01 16:18:32 +0800

    multiserver_support

--- src/main.rs
+++ src/main.rs
@@ -6,47 +6,69 @@ use serde_json;
 use std::{fs, thread};
 mod backend;
 mod frontend;
-#[derive(Deserialize)]
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+#[derive(Deserialize, Clone)]
 struct ConfigFile {
     addr: String,
     port: u16,
     length: u32,
     backend: BackendConfig,
-    frontend: FrontendConfig,
+    //frontend: FrontendConfig,
     servers: Vec<SingleServerConfig>,
 }
-#[derive(Debug, Deserialize)]
+/*#[derive(Debug, Deserialize, Clone)]
 struct FrontendConfig {
     addr: String,
     port: u16,
-}
-#[derive(Debug, Deserialize)]
+}*/
+#[derive(Debug, Deserialize, Clone)]
 struct BackendConfig {
     dbfile: String,
-    interval: u32,
+    //interval: u32,
 }
 #[derive(Debug, Deserialize, Clone)]
 struct SingleServerConfig {
     name: String,
-    addr: String,
+    //addr: String,
 }
-
-#[derive(Serialize)]
-struct ApiResponse {
-    code: u32,
-    message: String,
-    data: MonitorData,
+#[derive(Serialize, Debug)]
+struct SingleServerData {
+    timestamp: i64,
+    latency: i32,
+    player: i32,
+    playerlist: String,
 }
-#[derive(Serialize)]
-struct MonitorData {
-    color1: String,
-    current: String,
-    color2: String,
-    rate: String,
-    verboseinfo: String,
+impl Default for SingleServerData {
+    fn default() -> Self {
+        SingleServerData { timestamp: 0, latency: -1, player: -1, playerlist: "".to_string() }
+    }
+}
+#[derive(Serialize, Debug)]
+struct ResponseData {
+    data: Vec<SingleServerData>,
+}
+impl Default for ResponseData {
+    fn default() -> Self {
+        ResponseData { data: Vec::new() }
+    }
+}
+#[derive(Serialize, Debug)]
+struct ResponseList {
+    namelist: Vec<String>,
+}
+impl Default for ResponseList {
+    fn default() -> Self {
+        ResponseList { namelist: Vec::new() }
+    }
 }
 #[macro_use]
 extern crate rocket;
+
+static CONFIG: Lazy<Mutex<ConfigFile>> = Lazy::new(|| {
+    let conf = load_config();
+    return Mutex::new(conf)
+});
 
 fn load_config() -> ConfigFile {
     //let contents = fs::read_to_string("config.yaml").unwrap();
@@ -75,7 +97,14 @@ fn load_config() -> ConfigFile {
 
     return config;
 }
-fn get_record(filename: String, length: u32) -> (Vec<i64>, Vec<i32>, Vec<i32>) {
+
+/*fn advanced_round(value: f64, digits: u32) -> f64 {
+    let m = value * 10_f64.powi(digits as i32);
+    let r = m.round() / 10_f64.powi(digits as i32);
+    return r;
+}*/
+
+/*fn get_record(filename: String, length: u32) -> (Vec<i64>, Vec<i32>, Vec<i32>) {
     let conn = match Connection::open(filename) {
         Ok(r) => r,
         Err(e) => {
@@ -125,11 +154,7 @@ fn get_record(filename: String, length: u32) -> (Vec<i64>, Vec<i32>, Vec<i32>) {
     return (timestamps, latencys, players);
 }
 
-fn advanced_round(value: f64, digits: u32) -> f64 {
-    let m = value * 10_f64.powi(digits as i32);
-    let r = m.round() / 10_f64.powi(digits as i32);
-    return r;
-}
+
 
 fn load_lang(path: &str) -> serde_json::Value {
     let data = match fs::read_to_string(path) {
@@ -246,10 +271,83 @@ fn root_data() -> Json<ApiResponse> {
         message: "ok".to_string(),
         data: md,
     });
+}*/
+
+fn get_record(servername:String, filename: String, servers: Vec<SingleServerConfig>, length: u32) -> Result<ResponseData, String> {
+    let mut resp = ResponseData::default();
+    let conn = match Connection::open(filename) {
+        Ok(r) => r,
+        Err(e) => {
+            return Err(format!("Failed to open database {}", e));
+        }
+    };
+    let server = match servers.iter().find(|s| s.name == servername) {
+        Some(r) => r,
+        _ => {
+            return Err(format!("Invalid server name: {}", servername));
+        }
+    };
+
+    
+    let mut stmt = match conn.prepare(&format!(
+        "SELECT * FROM {} ORDER BY timestamp DESC LIMIT {}",
+        server.name,
+        length,
+    )) {
+        Ok(r) => r,
+        Err(e) => {
+            return Err(format!("Failed to read database {}", e));
+        }
+    };
+    let rows = match stmt
+    .query_map([], |row| {
+        Ok((
+            row.get::<_, i64>(0)?,
+            row.get::<_, i32>(1)?,
+            row.get::<_, i32>(2)?,
+            row.get::<_, String>(3)?,
+        ))
+    }) {
+        Ok(r) => r,
+        Err(e) => {
+            return Err(format!("Invalid db data {}", e));
+        }
+    };
+    //.unwrap();
+
+    for row in rows {
+        let mut single_resp = SingleServerData::default();
+        let (ctimestamp, clatency, cplayer, cplayerlist) = row.unwrap();
+        single_resp.timestamp = ctimestamp;
+        single_resp.latency = clatency;
+        single_resp.player = cplayer;
+        single_resp.playerlist = cplayerlist.to_string();
+        resp.data.push(single_resp);
+    }
+    return Ok(resp);
 }
 
-
-
+#[get("/api/servers/<servername>")]
+fn index_api_servers_servername(servername:String) -> Json<ResponseData> {
+    let conf = CONFIG.lock().unwrap().clone();
+    match get_record(servername, conf.backend.dbfile, conf.servers, conf.length) {
+        Ok(r) => {
+            return Json(r)
+        }
+        Err(e) => {
+            return Json(ResponseData { data: vec![SingleServerData{playerlist: e,..Default::default()}]})
+        }
+    }
+}
+#[get("/api/list")]
+fn index_api_list() -> Json<ResponseList> {
+    let conf = load_config();
+    let mut resp = ResponseList::default();
+    for server in conf.servers {
+        resp.namelist.push(server.name);
+    }
+    return Json(resp);
+}
 #[rocket::main]
 async fn main() {
     let conf = load_config();
@@ -289,7 +387,7 @@ async fn main() {
 
     let _ = rocket::custom(config)
         .attach(cors)
-        .mount("/", routes![root_data])
+        .mount("/", routes![index_api_servers_servername, index_api_list])
         .launch()
         .await
         .unwrap();
